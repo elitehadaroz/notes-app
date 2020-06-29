c2f543c774e23b4c7fdabb73c708f66305a416e9
elite <elitehadaroz@gmail.com>
2020-06-29 19:54:07 +0300

    oraginize sidebar items structure and keys

--- src/sidebaritem/sidebaritem.js
+++ src/sidebaritem/sidebaritem.js
@@ -9,12 +9,34 @@ import { removeHTMLTags } from '../helpers';
 
 class SidebarItemComponent extends React.Component {
 
-    constructor() {
-        super();
-    }
-
     render() {
-        return(<div>Hello from the sidebar item</div>)
+
+        const { _index, _note, _classes, selectedNoteIndex } = this.props;
+
+        return(
+            <div key={_index}>
+                <ListItem
+                    className={classes.listItem}
+                    selected={selectedNoteIndex === _index} //if selectedNoteIndex is equal to current index 
+                    alignItems='flex-start'>
+                        <div
+                            className={classes.textSection}
+                            onClick={() => this.selectNote(_note, _index)}>
+                                <ListItemText
+                                    primary={_note.title}
+
+                                    //note body is the entire text of the note - can be a massive string,
+                                    //so will take 30 first char from note text to show on the side bar:
+                                    secondary={_note.body.substring(0, 30) + '...'} 
+                                    > 
+
+                                </ListItemText>
+                            
+                        </div>
+
+                </ListItem>
+            </div>
+        )
     }
 }
 
