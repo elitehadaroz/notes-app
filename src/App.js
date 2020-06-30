cbc69b3613e07ccf51baaa18519b89e919dba9b0
elite <elitehadaroz@gmail.com>
2020-06-30 17:48:49 +0300

    creating new note function that will response to the user click on the new note btn. then will create a new note in firebase and then will show the new note on app and will wait for editing note body from user

--- src/App.js
+++ src/App.js
@@ -66,6 +66,27 @@ class App extends Component {
       timestamp: firebase.firestore.FieldValue.serverTimestamp() //firebase function that will generate a timestamp for us on the server
      });
   }
+
+  newNote = async (title) => {
+    const note = {
+      title: title,
+      body: ''
+    };
+
+    const newFromDB = await firebase
+      .firestore()
+      .collection('notes')
+      .add({ //add an item to the collection
+        title: note.title,
+        body: note.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp() //firebase function that will generate a timestamp for us on the server
+      });
+
+      const newID = newFromDB.id; // we'll need for access to id from firebase
+      await this.setState({ notes: [...this.state.notes, note] }); // updating the notes array with what is already in notes + the new note
+      const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]) //filter will return array of the note whoes id equal to newID
+      this.setState({ selectNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex});
+  }
 }
 
 export default App;
